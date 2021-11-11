b3530d09b03952b66a53d78ca337d5cf87e9b313
Julian Gautier <juliangautier@juliangautier.com>
2021-11-11 08:59:00 -0800

    start on machine, keyboard

--- src/machine.rs
+++ src/machine.rs
@@ -1,6 +1,13 @@
+use sdl2::keyboard::Keycode;
+use sdl2::Sdl;
 pub trait IO {
     fn input(&self, port: u8) -> u8;
     fn output(&mut self, port: u8, val: u8);
+}
+
+struct SpaceInvaders {
+    io: SpaceInvadersIO,
+    controller: dyn Controller
 }
 
 pub struct SpaceInvadersIO {
@@ -23,7 +30,6 @@ impl SpaceInvadersIO {
             port2: 0b0000_0000
         }
     }
-    
 }
 
 impl IO for SpaceInvadersIO {
@@ -45,9 +51,70 @@ impl IO for SpaceInvadersIO {
             3 | 5 | 6 => {},
             _ => panic!("cannot write to port {}", port)
         }
-
     }
 }
+
+pub enum Button {
+    Shoot,
+    Left,
+    Right
+}
+
+pub enum Player {
+    Player1,
+    Player2
+}
+
+pub struct ButtonPress {
+    button: Button,
+    player: Player
+}
+
+/*pub struct Controller {
+    button_presses: Vec<(Player, Button)>
+}
+*/
+
+pub trait Controller {
+    fn get_button_presses(&self) -> Vec<(Player, Button)>;
+}
+
+pub struct Sdl2KeyboardController {
+    event_pump: sdl2::EventPump
+}
+
+impl Sdl2KeyboardController {
+    pub fn new(sdl_context: Sdl) -> Self {
+        Sdl2KeyboardController {
+            event_pump: sdl_context.event_pump().unwrap()
+        }
+    }
+}
+
+impl Controller for Sdl2KeyboardController {
+    fn get_button_presses(&self) -> Vec<(Player, Button)> {
+        self.event_pump.keyboard_state().pressed_scancodes()
+            .filter_map(|scan_code| {
+                let key_code = Keycode::from_scancode(scan_code)?;
+                match key_code {
+                    Keycode::Left => {
+                        Some((Player::Player1, Button::Left))
+                    }
+                    Keycode::Right => {
+                        Some((Player::Player1, Button::Right))
+                    }
+                    Keycode::Space => {
+                        Some((Player::Player1, Button::Shoot))
+                    }
+                    _ => {
+                        None
+                    }
+                }
+            })
+            .collect()
+    }
+}
+
 
 #[cfg(test)]
 mod test {
